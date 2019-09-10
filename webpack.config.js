3c153a7f77e5551c5998f3e06decf7efc8d5eb4f
hulahulahhh <1002340239@qq.com>
2019-09-10 19:14:30 +0800

    feat: format webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,75 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: '/dist' // 将dist目录下的文件作为可访问文件, 但若dist目录没有内容呢，wds打包后的文件不是放在内存中的吗，那为什么要设置contentBase呢？
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => {
-                                require('autoprefixer')({
-                                    overrideBrowserslist: ['last 2 version', '>1%', 'ios 7']
-                                })
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'img/[name].[ext]'
-                        }
-                    }
-                ]
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "/dist" // 将dist目录下的文件作为可访问文件, 但若dist目录没有内容呢，wds打包后的文件不是放在内存中的吗，那为什么要设置contentBase呢？
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => {
+                require("autoprefixer")({
+                  overrideBrowserslist: ["last 2 version", ">1%", "ios 7"]
+                });
+              }
             }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'webpack configure',
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCSS: true,
-                minifyJS: true,
-                removeComments: true
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name].[ext]"
             }
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name]_[contenthash:8].css'
-        }),
-        new OptimizeCssAssetsWebpackPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano')
-        })
+          }
+        ]
+      }
     ]
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack configure",
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name]_[contenthash:8].css"
+    }),
+    new OptimizeCssAssetsWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano")
+    })
+  ]
+};
